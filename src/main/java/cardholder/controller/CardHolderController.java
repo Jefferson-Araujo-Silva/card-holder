0f7861549cc7478c113b068b0f34d472e89443b0
Jefferson <jefferson.silva@jazztech.com.br>
2023-06-29 14:22:53 -0300

    adding new endpoint get for consulting card holders

--- src/main/java/cardholder/controller/CardHolderController.java
+++ src/main/java/cardholder/controller/CardHolderController.java
@@ -3,14 +3,17 @@ package cardholder.controller;
 import cardholder.controller.request.CardHolderRequest;
 import cardholder.controller.response.CardHolderResponse;
 import cardholder.service.CardHolderService;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +30,17 @@ public class CardHolderController {
         MDC.put("correlationId", UUID.randomUUID().toString());
         LOGGER.info("Received an post requisition at endpoint %s".formatted(PATH_DEFAULT_ENDPOINT));
         return cardHolderService.createNewCardHolder(request);
+    }
+    @GetMapping
+    public List<CardHolderResponse> getCardHolderBy(@RequestParam(value = "activeStatus", required = false) String activeStatus) {
+        if (activeStatus != null) {
+            MDC.put("correlationId", UUID.randomUUID().toString());
+            LOGGER.info(
+                    "Received an post requisition at get endpoint %s with filter ?activeStatus=%s".formatted(PATH_DEFAULT_ENDPOINT, activeStatus));
+            return cardHolderService.getCardHolderByStatus(activeStatus);
+        }
+        MDC.put("correlationId", UUID.randomUUID().toString());
+        LOGGER.info("Received an post requisition at get endpoint %s".formatted(PATH_DEFAULT_ENDPOINT));
+        return cardHolderService.getAllCardHolders();
     }
 }
