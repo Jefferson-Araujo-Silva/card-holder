967571f54c9dd532ef3a7414d5ad586b6030cc03
Jefferson <jefferson.silva@jazztech.com.br>
2023-06-29 11:51:03 -0300

    adding project initial structure

---
+++ src/test/java/com/cardholder/app/CardHolderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardholder.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardHolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
